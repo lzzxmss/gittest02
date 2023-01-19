ca29df333374228ee1d79743a6f784ced80ae7dc
lzzxmss <1595873520@qq.com>
2023-01-19 16:19:12 +0800

    second commit

--- src/main/java/cn/xmss/TestGit.java
+++ src/main/java/cn/xmss/TestGit.java
@@ -3,5 +3,6 @@ package cn.xmss;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello GitHub!");
     }
 }
